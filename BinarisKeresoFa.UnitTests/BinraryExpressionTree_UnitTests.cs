374f5e4244c0d21822cbecfcfe9323410972954b
TorokDan <dani.torok8@gmail.com>
2022-04-08 10:26:53 +0200

    A feladatban lévő tesztesetek felvétele.

--- BinarisKeresoFa.UnitTests/BinraryExpressionTree_UnitTests.cs
+++ BinarisKeresoFa.UnitTests/BinraryExpressionTree_UnitTests.cs
@@ -12,8 +12,17 @@ namespace BinarisKeresoFa.UnitTests
         }
 
         [Test]
-        [TestCase("2+3", "23+")]
-        [TestCase("2+3*4", "23+4*")]
+        [TestCase("2", "2", TestName = "only node")]
+        [TestCase("2+3", "23+", TestName = "Simple operation")]
+        [TestCase("2-3", "23-", TestName = "Simple operation")]
+        [TestCase("2*3", "23*", TestName = "Simple operation")]
+        [TestCase("2/3", "23/", TestName = "Simple operation")]
+        [TestCase("2^3", "23^", TestName = "Simple operation")]
+        [TestCase("(2+(3*4))", "234*+", TestName = "Example operation")]
+        [TestCase("((2*3)+4)", "23*4+", TestName = "Example operation")]
+        [TestCase("((2*3)+(4*5))", "23*45*+", TestName = "Example operation")]
+        [TestCase("((2+3)/(4-5))", "23+45-/", TestName = "Example operation")]
+        [TestCase("((((2+3)*4)+5)/((6^7)+8))", "23+4*5+67^8+/", TestName = "Example operation")]
         public void Build(string data, string expRes)
         {
             Assert.AreEqual( expRes, BinaryExpressionTree.Build(data).ToString());
